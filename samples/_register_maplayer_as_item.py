c88cbeb4c1bff6a0151f5aa5d96cf03a6da48524
Michael Miller <mmiller@esri.com>
2014-09-10 14:49:05 -0400

    Updated Name

--- samples/_register_maplayer_as_item.py
+++ samples/_register_maplayer_as_item.py
@@ -12,19 +12,19 @@ if __name__ == "__main__":
     try:
         agol = admin.AGOL(username='',password='')
         inparams = {"url":"http://IPADDRESS/arcgis/rest/services/Sanitary_Sewer_Infrastructure/Sanitary_Sewer_Network/MapServer/6"}
-        
-        res = agol.addItem(name="TestForMark", 
-                     tags="Test", 
-                     description="Test", 
-                     snippet="test", 
-                     data=None, 
-                     extent="-88.1799,41.7718,-88.1353,41.7823", 
+
+        res = agol.addItem(name="SampleMapLayer",
+                     tags="Test",
+                     description="Test",
+                     snippet="test",
+                     data=None,
+                     extent="-88.1799,41.7718,-88.1353,41.7823",
                      item_type="Feature Service",
                      folder=None,
                      inparams=inparams,
                      typeKeywords=["Data", "Service", "Feature Service", "ArcGIS Server", "Feature Access"]
                     )
-                
-        print res 
+
+        print res
     except ValueError, e:
         print e
