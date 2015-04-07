4f911ea2772bd03df080c32d5b31e12497e3466c
Andrew <achapkowski@esri.com>
2015-04-07 09:38:30 -0400

    renamed imageservice.py to _imageservice.py and uploads.py to _uploads.py, modified __init__.py to limit what is returned in the ags pacakge.

--- src/arcrest/ags/__init__.py
+++ src/arcrest/ags/__init__.py
@@ -2,10 +2,10 @@ from featureservice import *
 from mapservice import *
 from layer import *
 from geoprocessing import *
-from imageservice import *
-from uploads import *
+from _imageservice import ImageService
+from _uploads import Uploads
 from _globeservice import GlobeService, GlobeServiceLayer
 from _mobileservice import MobileService, MobileServiceLayer
 from _geodataservice import GeoDataService
 from _geocodeservice import GeocodeService
-__version__ = "2.0.100"
+__version__ = "2.0.120"
