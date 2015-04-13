eee7d89a834679244e138038f6736d536652d129
Andrew <achapkowski@esri.com>
2015-04-13 08:32:45 -0400

    added optional request support on all web operations. If requests is present, then the POST/GET operations default to those libraries, if not, it falls back to the standard library.

--- src/arcrest/web/_base.py
+++ src/arcrest/web/_base.py
@@ -11,6 +11,11 @@ import mimetypes
 import mimetools
 from cStringIO import StringIO
 import re
+try:
+    import requests
+    allowRequests = True
+except:
+    allowRequests = False
 class AGOLRedirectHandler(urllib2.HTTPRedirectHandler):
     def http_error_301(self, req, fp, code, msg, headers):
         result = urllib2.HTTPRedirectHandler.http_error_301(
@@ -34,7 +39,321 @@ class BaseWebOperations(object):
     _proxy_url = None
     _proxy_port = None
     #----------------------------------------------------------------------
-    def _download_file(self, url, save_path, file_name=None, param_dict=None,proxy_url=None, proxy_port=None):
+    def _do_get(self, url, param_dict,
+                header=None, proxy_url=None,
+                proxy_port=None,compress=True,
+                forceStandardLib=False):
+        if allowRequests and forceStandardLib == False:
+            return self._do_get_requests(url=url,
+                                         param_dict=param_dict,
+                                         header=header,
+                                         proxy_url=proxy_url,
+                                         proxy_port=proxy_port,
+                                         compress=compress)
+        else:
+            return self._do_get_stnd(url=url,
+                                     param_dict=param_dict,
+                                     header=header,
+                                     proxy_url=proxy_url,
+                                     proxy_port=proxy_port,
+                                     compress=compress)
+    #----------------------------------------------------------------------
+    def _do_post(self, url, param_dict,
+                 proxy_url=None, proxy_port=None,
+                 header={},
+                 forceStandardLib=False):
+        """
+        performs the POST operation and returns dictionary result
+
+        Inputs:
+           url - web address
+           param_dict - dictionary object that carriers the REST parameters
+           proxy_url - web address/IP of proxy
+           proxy_port - web port of proxy
+           header - optional headers
+           forceStandardLib - optional boolean that make the calls force to use urllib libraries
+             regardless of the precence of requests of not.  The default is false.
+        """
+        if allowRequests and forceStandardLib == False:
+            return self._do_post_requests(url=url,
+                                          param_dict=param_dict,
+                                          proxy_url=proxy_url,
+                                          proxy_port=proxy_port,
+                                          header=header)
+        else:
+            return self._do_post_stnd(url=url,
+                                      param_dict=param_dict,
+                                      proxy_url=proxy_url,
+                                      proxy_port=proxy_port,
+                                      header=header)
+    #----------------------------------------------------------------------
+    def _download_file(self, url, save_path,
+                       file_name=None, param_dict=None,
+                       proxy_url=None, proxy_port=None,
+                       forceStandardLib=False):
+        """
+        Downloads a file using either the standard urllib library or
+        the optional request library for python.
+        """
+        if allowRequests and forceStandardLib == False:
+            return self._download_file_requests(url=url,
+                                         save_path=save_path,
+                                         file_name=file_name,
+                                         param_dict=param_dict,
+                                         proxy_url=proxy_url,
+                                         proxy_port=proxy_port)
+        else:
+            return self._download_file_stnd(url=url,
+                                         save_path=save_path,
+                                         file_name=file_name,
+                                         param_dict=param_dict,
+                                         proxy_url=proxy_url,
+                                         proxy_port=proxy_port)
+    #----------------------------------------------------------------------------------
+    def _post_multipart(self, host, selector,
+                        fields, files,
+                        ssl=False,port=80,
+                        proxy_url=None,proxy_port=None,
+                        forceStandardLib=False):
+        """ performs a multi-post to AGOL, Portal, or AGS
+            Inputs:
+               host - string - root url (no http:// or https://)
+                   ex: www.arcgis.com
+               selector - string - everything after the host
+                   ex: /PWJUSsdoJDp7SgLj/arcgis/rest/services/GridIndexFeatures/FeatureServer/0/1/addAttachment
+               fields - dictionary - additional parameters like token and format information
+               files - tuple array- tuple with the file name type, filename, full path
+               ssl - option to use SSL
+               proxy_url - string - url to proxy server
+               proxy_port - interger - port value if not on port 80
+
+            Output:
+               JSON response as dictionary
+            Useage:
+               import urlparse
+               url = "http://sampleserver3.arcgisonline.com/ArcGIS/rest/services/SanFrancisco/311Incidents/FeatureServer/0/10261291"
+               parsed_url = urlparse.urlparse(url)
+               params = {"f":"json"}
+               print _post_multipart(host=parsed_url.hostname,
+                               selector=parsed_url.path,
+                               files=files,
+                               fields=params
+                               )
+        """
+        if allowRequests and forceStandardLib == False:
+            return self._post_multipart_requests(host=host,
+                                                 selector=selector,
+                                                 fields=fields,
+                                                files=files,
+                                                ssl=ssl,
+                                                port=port,
+                                                proxy_url=proxy_url,
+                                                proxy_port=proxy_port)
+        else:
+            return self._post_multipart_stnd(host=host,
+                                     selector=selector,
+                                     fields=fields,
+                                     files=files,
+                                     ssl=ssl,
+                                     port=port,
+                                     proxy_url=proxy_url,
+                                     proxy_port=proxy_port)
+    #----------------------------------------------------------------------
+    def _download_file_requests(self,
+                                url,
+                                save_path,
+                                file_name=None,
+                                param_dict=None,
+                                proxy_url=None,
+                                proxy_port=None):
+        """
+           downloads a file using requests
+
+           Inputs:
+             url - web address to download file
+             save_path - folder on disk to save content
+             file_name - optional - name of the file + extension
+                ex: test.txt
+            param_dict - python dictionary of Key/Value pairs.  Contains
+             things like token, format, etc..
+            proxy_url - optional - IP of proxy server
+            proxy_port - optional - port of proxy server if the proxy_url
+             is given, and the proxy_port is none, the function assumes the
+             proxy_port is 80.
+
+            returns:
+               string path to file.
+        """
+        proxies = None
+        if not proxy_url is None:
+            if proxy_port is None:
+                proxy_port = 80
+            proxies = {
+                "http": "%s:%s" % (proxy_url, proxy_port),
+                "https": "%s:%s" % (proxy_url, proxy_port),
+            }
+        r = requests.get(url,
+                         stream=True,
+                         params=param_dict,
+                         proxies=proxies)
+        if file_name is None:
+            file_name = r.headers['Content-Disposition']
+        save_file = os.path.join(save_path, file_name)
+        with open(save_file, 'wb') as f:
+            for chunk in r.iter_content(chunk_size=1024):
+                if chunk:
+                    f.write(chunk)
+                f.flush()
+        return save_file
+    #----------------------------------------------------------------------
+    def _do_post_requests(self, url, param_dict, proxy_url=None, proxy_port=None, header={}):
+        """
+           Performs the POST operation using the requests library
+        """
+        proxies = None
+        if not proxy_url is None:
+            if proxy_port is None:
+                proxy_port = 80
+            proxies = {
+                "http": "%s:%s" % (proxy_url, proxy_port),
+                "https": "%s:%s" % (proxy_url, proxy_port),
+            }
+        headers = {'Referer': self._referer_url,
+                   'User-Agent': self._useragent,
+                   'Accept-Encoding': ''}
+        if len(header) > 0 :
+            headers = dict(headers.items() + header.items())
+        content = requests.post(url=url,
+                                data=param_dict,
+                                json=None,
+                                proxies=proxies,
+                                headers=headers,
+                                verify=False).content
+        jres = json.loads(content)
+        if 'error' in jres:
+            if jres['error']['message'] == 'Request not made over ssl':
+                if url.startswith('http://'):
+                    url = url.replace('http://', 'https://')
+                    return self._do_post_requests( url, param_dict,
+                                                   proxy_url, proxy_port,
+                                                   header=header)
+        return jres
+    #----------------------------------------------------------------------
+    def _do_get_requests(self, url, param_dict, header=None, proxy_url=None, proxy_port=None,compress=True):
+        """performs a GET request using requests library"""
+        proxies = None
+        if not proxy_url is None:
+            if proxy_port is None:
+                proxy_port = 80
+            proxies = {
+                "http": "%s:%s" % (proxy_url, proxy_port),
+                "https": "%s:%s" % (proxy_url, proxy_port),
+            }
+        headers = {
+            'Referer': self._referer_url,
+            'User-Agent': self._useragent
+        }
+        if not header is None  and \
+           len(header.items()) > 0:
+            for k,v in header.iteritem():
+                headers[k] = v
+        if compress:
+            headers['Content-Encoding'] = 'gzip'
+        content = requests.get(url=url,
+                               params=param_dict,
+                               proxies=proxies,
+                               headers=headers,
+                               verify=False).content
+        jres = json.loads(content)
+        if 'error' in jres:
+            if jres['error']['message'] == 'Request not made over ssl':
+                if url.startswith('http://'):
+                    url = url.replace('http://', 'https://')
+                    return self._do_get_requests(url,
+                                                 param_dict,
+                                                 headers,
+                                                 proxy_url,
+                                                 proxy_port,
+                                                 compress)
+        return jres
+    #----------------------------------------------------------------------
+    def _post_multipart_requests(self,
+                                 host,
+                                 selector,
+                                 fields,
+                                 files,
+                                 ssl=False,
+                                 port=80,
+                                 proxy_url=None,
+                                 proxy_port=None):
+        """ performs a multi-post to AGOL, Portal, or AGS
+            Inputs:
+               host - string - root url (no http:// or https://)
+                   ex: www.arcgis.com
+               selector - string - everything after the host
+                   ex: /PWJUSsdoJDp7SgLj/arcgis/rest/services/GridIndexFeatures/FeatureServer/0/1/addAttachment
+               fields - dictionary - additional parameters like token and format information
+               files - tuple array- tuple with the file name type, filename, full path
+                 ex: [('file', r"c:\temp\test.txt", "text.txt")]
+               ssl - option to use SSL
+               proxy_url - string - url to proxy server
+               proxy_port - interger - port value if not on port 80
+
+            Output:
+               JSON response as dictionary
+            Useage:
+               import urlparse
+               url = "http://sampleserver3.arcgisonline.com/ArcGIS/rest/services/SanFrancisco/311Incidents/FeatureServer/0/10261291"
+               parsed_url = urlparse.urlparse(url)
+               params = {"f":"json"}
+               print _post_multipart(host=parsed_url.hostname,
+                               selector=parsed_url.path,
+                               files=files,
+                               fields=params
+                               )
+        """
+        verify = False
+        proxies = None
+        headers = {'User-agent': 'ArcREST'}
+        postFiles = {}
+        if ssl:
+            url = "https://%s%s" % (host, selector)
+        else:
+            url = "http://%s%s" % (host, selector)
+        if proxy_url is not None:
+            if proxy_port is None:
+                proxy_port = 80
+            proxies = {"http":"http://%s:%s" % (proxy_url, proxy_port),
+                       "https":"https://%s:%s" % (proxy_url, proxy_port)}
+        #files come in as name, full file path, file name
+        # ex: [('file', r"c:\temp\test.txt", "text.txt")]
+        for f in files:
+            postFiles[f[0]] = open(f[1], 'rb').read()
+            del f
+        del files
+        content = requests.post(url=url,
+                            data=fields,
+                            json=None,
+                            proxies=proxies,
+                            headers=headers,
+                            files=postFiles,
+                            verify=verify).content
+        jres = json.loads(content)
+        if 'error' in jres:
+            if jres['error']['message'] == 'Request not made over ssl':
+                if url.startswith('http://'):
+                    url = url.replace('http://', 'https://')
+                    return self._post_multipart_requests(host,
+                                                         selector,
+                                                         fields,
+                                                         files,
+                                                         ssl,
+                                                         port,
+                                                         proxy_url,
+                                                         proxy_port)
+        return jres
+    #----------------------------------------------------------------------
+    def _download_file_stnd(self, url, save_path, file_name=None, param_dict=None,proxy_url=None, proxy_port=None):
         """ downloads a file """
         try:
             #if url.find("http://") > -1:
@@ -96,7 +415,7 @@ class BaseWebOperations(object):
             print "URL Error:",e.reason , url
             return False
     #----------------------------------------------------------------------
-    def _do_post(self, url, param_dict, proxy_url=None, proxy_port=None, header={}):
+    def _do_post_stnd(self, url, param_dict, proxy_url=None, proxy_port=None, header={}):
         """ performs the POST operation and returns dictionary result """
         if proxy_url is not None:
             if proxy_port is None:
@@ -134,7 +453,7 @@ class BaseWebOperations(object):
 
         return self._unicode_convert(jres)
     #----------------------------------------------------------------------
-    def _do_get(self, url, param_dict, header=None, proxy_url=None, proxy_port=None,compress=True):
+    def _do_get_stnd(self, url, param_dict, header=None, proxy_url=None, proxy_port=None,compress=True):
         """ performs a get operation """
         format_url = url + "?%s" % urllib.urlencode(param_dict)
 
@@ -200,11 +519,11 @@ class BaseWebOperations(object):
             mntype= "File/%s" % fileExtension.replace('.', '')
         return mntype
     #----------------------------------------------------------------------------------
-    def _post_multipart(self, host, selector,
+    def _post_multipart_stnd(self, host, selector,
                         fields, files,
                         ssl=False,port=80,
                         proxy_url=None,proxy_port=None):
-        """ performs a multi-post to AGOL, Portal, or AGS
+        """ performs a multi-post to AGOL, Portal, or AGS using standard library
             Inputs:
                host - string - root url (no http:// or https://)
                    ex: www.arcgis.com
