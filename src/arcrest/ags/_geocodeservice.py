1257c2d0a4769a364935a531b04bb2dce9d32467
Andrew <achapkowski@esri.com>
2015-04-20 07:49:34 -0400

    Added proper GeocodeService description to the class.

--- src/arcrest/ags/_geocodeservice.py
+++ src/arcrest/ags/_geocodeservice.py
@@ -5,7 +5,14 @@ import json, types
 ########################################################################
 class GeocodeService(BaseAGSServer):
     """
-    Represents a single mobile service layer
+    Geocoding is the process of assigning a location, usually in the form
+    of coordinate values (points), to an address by comparing the
+    descriptive location elements in the address to those present in the
+    reference material. Addresses come in many forms, ranging from the
+    common address format of a house number followed by the street name and
+    succeeding information, to other location descriptions such as postal
+    zone or census tract. An address includes any type of information that
+    distinguishes a place.
     """
     _url = None
     _proxy_url = None
