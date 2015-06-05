99563652cd1ed43bfc46b1017c56f2d410327c25
Brian James Baldwin <BrianJBaldwin@gmail.com>
2015-06-05 14:12:43 -0700

    Update __init__.py
    Just pushing the original code back without the comment, Ill add the comment to the issues.
    
    Was just trying to add a comment directly to this line of code, but not sure how to elegantly do it.

--- src/arcrest/agol/__init__.py
+++ src/arcrest/agol/__init__.py
@@ -1,6 +1,6 @@
 from featureservice import *
 from layer import *
 from tiledservice import *
-import helperservices  ## This is throwing an error right now, no module named helperservices
+import helperservices
 
 __version__ = "2.0.120"
