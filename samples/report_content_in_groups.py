07ce0b1b69885a3b197e29e53f023913a152d674
Michael Miller <mmiller@esri.com>
2015-06-12 11:24:24 -0400

    updated group reporter sample

--- samples/report_content_in_groups.py
+++ samples/report_content_in_groups.py
@@ -6,6 +6,7 @@ import arcrest
 import os,io
 import json
 from arcresthelper import orgtools
+import csv
 def trace():
     """
         trace finds the line, the filename
@@ -24,43 +25,48 @@ def trace():
     return line, filename, synerror
 
 if __name__ == "__main__":
-    username = "<username>"
-    password = "<password>"
-    url = "<portal or AGOL url>"
-    groups = "<[groups, group1]>"
-    outputlocation = "<Location to store the results>"
-    outputfilename = "<outputfilename.json>"
-    
+    username = "MikeSolutions"
+    password = "double1pa"
+    url = "http://www.arcgis.com"
+    groups = ["Network Services"] #Name of groupd
+    outputlocation = r"C:\TEMP"
+    outputfilename = "group.json"
+    outputitemID = "id.csv"
     try:
 
-                            
-        groups = []
         orgt = orgtools.orgtools(username = username, password=password,org_url=url,
                                  token_url=None, 
                                  proxy_url=None, 
                                  proxy_port=None)
 
-
+        groupRes = []
         if orgt.valid:
             fileName = os.path.join(outputlocation,outputfilename)
+            csvFile = os.path.join(outputlocation,outputitemID)
             iconPath = os.path.join(outputlocation,"icons")  
             if not os.path.exists(iconPath):
                 os.makedirs(iconPath)                                                    
-            file = io.open(fileName, "w", encoding='utf-8')                                               
-            for groupName in searchgroups:
-                results = orgt.getGroupContent(groupName=groupName)  
-               
-                if not results is None and 'results' in results:
-                  
-                    for result in results['results']:
-                        thumbLocal = orgt.getThumbnailForItem(itemId=result['id'],fileName=result['title'],filePath=iconPath)
-                        result['thumbnail']=thumbLocal
-                        groups.append(result)
-                       
-            if len(groups) > 0:
-                print "%s items found" % str(len(groups))
-                file.write(unicode(json.dumps(groups, ensure_ascii=False, sort_keys=True, indent=4, separators=(',', ': '))))                                              
-            file.close()                                                                                    
+            file = io.open(fileName, "w", encoding='utf-8')  
+            with open(csvFile, 'wb') as csvfile:
+                idwriter = csv.writer(csvfile, delimiter=',',
+                            quotechar='|', quoting=csv.QUOTE_MINIMAL)                
+                for groupName in groups:
+                    results = orgt.getGroupContent(groupName=groupName)  
+                   
+                    if not results is None and 'results' in results:
+
+                      
+                        for result in results['results']:
+                            idwriter.writerow([result['title'],result['id']])
+                            thumbLocal = orgt.getThumbnailForItem(itemId=result['id'],fileName=result['title'],filePath=iconPath)
+                            result['thumbnail']=thumbLocal
+                            groupRes.append(result)
+                           
+                if len(groupRes) > 0:
+                    print "%s items found" % str(len(groupRes))
+                    file.write(unicode(json.dumps(groupRes, ensure_ascii=False, sort_keys=True, indent=4, separators=(',', ': '))))                                              
+            file.close()
+            
     except:
         line, filename, synerror = trace()
         print("error on line: %s" % line)
