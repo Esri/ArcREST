c25727fcb4e97d588dc99518afb8789da86184c8
Andrew <achapkowski@esri.com>
2015-09-09 07:40:47 -0400

    Added sample to show how to change the URL scheme from http to https.

---
+++ samples/Change Map Service Scheme.py
@@ -0,0 +1,33 @@
+"""
+   This sample shows users how to add a map service via URL, then
+   update that item's URL from http scheme to https.
+
+   This method will work for both Portal and ArcGIS Online
+"""
+import arcrest
+
+if __name__ == "__main__":
+    username = "<username>"
+    password = "<password>"
+    org_url = "<portal org url>"
+    url = "http://sampleserver6.arcgisonline.com/arcgis/rest/services/MontgomeryQuarters/MapServer"
+    #  Create a security handler for portal
+    #
+    sh = arcrest.PortalTokenSecurityHandler(username=username,
+                                            password=password,
+                                            org_url=org_url)
+    #  Access the admin side of the site
+    #
+    admin = arcrest.manageorg.Administration(url=org_url, securityHandler=sh)
+    content = admin.content
+    users = content.users
+    user = users.user(username=username)
+    ip = arcrest.manageorg.ItemParameter()
+    ip.title = "MontgomeryQuarters"
+    ip.description = "Represents a simple map service"
+    ip.tags = "MapService"
+    ip.type = "Map Service"
+    item = user.addItem(itemParameters=ip,
+                       url=url)
+    item.updateItem(itemParameters=arcrest.manageorg.ItemParameter(), serviceUrl=url.replace("http://", "https://") + "#")
+    item.updateItem(itemParameters=arcrest.manageorg.ItemParameter(), serviceUrl=url.replace("http://", "https://"))
