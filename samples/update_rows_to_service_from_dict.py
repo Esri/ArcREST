2c6c9f03dd2768c0863fb77eed2d551164b86dd9
Michael Miller <mmiller@esri.com>
2015-10-22 13:51:36 -0400

    forgot sample

---
+++ samples/update_rows_to_service_from_dict.py
@@ -0,0 +1,54 @@
+
+"""
+   This sample shows how update a row based on its objectID
+"""
+
+from arcresthelper import securityhandlerhelper
+from arcrest.agol import FeatureLayer
+from arcrest.common.general import Feature
+
+if __name__ == "__main__":
+    url = ''# URL to Service
+    proxy_port = None
+    proxy_url = None    
+
+    securityinfo = {}
+    securityinfo['security_type'] = 'Portal'#LDAP, NTLM, OAuth, Portal, PKI, ArcGIS
+    securityinfo['username'] = "" #User Name
+    securityinfo['password'] = "" #Password
+    securityinfo['org_url'] = "http://www.arcgis.com"
+    securityinfo['proxy_url'] = proxy_url
+    securityinfo['proxy_port'] = proxy_port
+    securityinfo['referer_url'] = None
+    securityinfo['token_url'] = None
+    securityinfo['certificatefile'] = None
+    securityinfo['keyfile'] = None
+    securityinfo['client_id'] = None
+    securityinfo['secret_id'] = None   
+   
+    shh = securityhandlerhelper.securityhandlerhelper(securityinfo=securityinfo)
+    if shh.valid == False:
+        print shh.message
+    else:
+        fl= FeatureLayer(
+            url=url,
+            securityHandler=shh.securityhandler,
+            proxy_port=proxy_port,
+            proxy_url=proxy_url,
+            initialize=True)
+        
+        features = []
+        json_string={'geometry': 
+                        {
+                            'y': 1885855.2531960313, 
+                            'x': 1034495.0035156211}
+                        ,
+                     'attributes': 
+                     {  'NAME': 'NameChange', 
+                        'OBJECTID': 1
+                     }}
+
+        features.append(Feature(json_string=json_string))
+      
+       
+        print fl.updateFeature(features=features)
