04ed9028ed3b2111e45e5bd2e59864be04dd6ee5
AlexArcPy <AlexArcPy@users.noreply.github.com>
2015-11-11 18:16:08 +0100

    Added Version class in _geodataservice.py
    GeoDataServer instance will have versions property with a list of Version objects (each containing attributes found at http://localhost:6080/arcgis/rest/services/%ServiceName%/GeoDataServer/versions/sde.DEFAULT).

--- src/arcrest/ags/_geodataservice.py
+++ src/arcrest/ags/_geodataservice.py
@@ -48,7 +48,17 @@ class GeoDataService(BaseAGSServer):
                       not attr.startswith('_')]
         for k,v in json_dict.iteritems():
             if k in attributes:
-                setattr(self, "_"+ k, v)
+                if k == "versions" and json_dict[k]:
+                    self._versions = []
+                    for version in v:
+                        self._versions.append(
+                            Version(url=self._url + "/versions/%s" % version,
+                                    securityHandler=self._securityHandler,
+                                    proxy_url=self._proxy_url,
+                                    proxy_port=self._proxy_port,
+                                    initialize=False))
+                else:
+                    setattr(self, "_"+ k, v)
             else:
                 print k, " - attribute not implemented for GeoData Service"
     #----------------------------------------------------------------------
@@ -100,3 +110,116 @@ class GeoDataService(BaseAGSServer):
         if self._versions is None:
             self.__init()
         return self._versions
+
+########################################################################
+class Version(BaseAGSServer):
+    """represents a version in a geodata service"""
+
+    _securityHandler = None
+    _proxy_port = None
+    _proxy_url = None
+    _url = None
+
+    _name = None
+    _description = None
+    _created = None
+    _modified = None
+    _access = None
+    _parentVersion = None
+    _childVersions = None
+    _ancestorVersions = None
+    #----------------------------------------------------------------------
+    def __init__(self, url, securityHandler=None,
+                 proxy_url=None, proxy_port=None,
+                 initialize=False):
+        """Constructor"""
+        self._url = url
+        self._securityHandler = securityHandler
+        if not securityHandler is None:
+            self._referer_url = securityHandler.referer_url
+        self._proxy_port = proxy_port
+        self._proxy_url = proxy_url
+        if initialize:
+            self.__init()
+    #----------------------------------------------------------------------
+    def __init(self):
+        """ initializes all the properties """
+        params = {
+            "f" : "json"
+        }
+        json_dict = self._do_get(url=self._url, param_dict=params,
+                                 securityHandler=self._securityHandler,
+                                 proxy_url=self._proxy_url,
+                                 proxy_port=self._proxy_port)
+        attributes = [attr for attr in dir(self)
+                      if not attr.startswith('__') and \
+                      not attr.startswith('_')]
+        for k,v in json_dict.iteritems():
+            if k in attributes:
+                setattr(self, "_"+ k, json_dict[k])
+            else:
+                print k, " - attribute not implemented in Version."
+
+    #----------------------------------------------------------------------
+    def __str__(self):
+        """returns object as string"""
+        if self._json is None:
+            self.__init()
+        return self._json
+    #----------------------------------------------------------------------
+    def __iter__(self):
+        """
+        returns key/value pair
+        """
+        attributes = json.loads(str(self))
+        for att in attributes.keys():
+            yield [att, getattr(self, att)]
+
+    #----------------------------------------------------------------------
+    @property
+    def name(self):
+        if self._name is None:
+            self.__init()
+        return self._name
+    #----------------------------------------------------------------------
+    @property
+    def description(self):
+        if self._description is None:
+            self.__init()
+        return self._description
+    #----------------------------------------------------------------------
+    @property
+    def created(self):
+        if self._created is None:
+            self.__init()
+        return self._created
+    #----------------------------------------------------------------------
+    @property
+    def modified(self):
+        if self._modified is None:
+            self.__init()
+        return self._modified
+    #----------------------------------------------------------------------
+    @property
+    def access(self):
+        if self._access is None:
+            self.__init()
+        return self._access
+    #----------------------------------------------------------------------
+    @property
+    def parentVersion(self):
+        if self._parentVersion is None:
+            self.__init()
+        return self._parentVersion
+    #----------------------------------------------------------------------
+    @property
+    def childVersions(self):
+        if self._childVersions is None:
+            self.__init()
+        return self._childVersions
+    #----------------------------------------------------------------------
+    @property
+    def ancestorVersions(self):
+        if self._ancestorVersions is None:
+            self.__init()
+        return self._ancestorVersions
