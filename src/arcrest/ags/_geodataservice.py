294a413f5e5bed9786d321377d35207056e0fad8
AlexArcPy <AlexArcPy@users.noreply.github.com>
2015-11-12 13:18:05 +0100

    Implemented Replica class in _geodataservice.py
    geodata service replicas property will now return a list of Replica objects (with all their attributes)

--- src/arcrest/ags/_geodataservice.py
+++ src/arcrest/ags/_geodataservice.py
@@ -57,6 +57,15 @@ class GeoDataService(BaseAGSServer):
                                     proxy_url=self._proxy_url,
                                     proxy_port=self._proxy_port,
                                     initialize=False))
+                elif k == "replicas" and json_dict[k]:
+                    self._replicas = []
+                    for version in v:
+                        self._replicas.append(
+                            Replica(url=self._url + "/replicas/%s" % version,
+                                    securityHandler=self._securityHandler,
+                                    proxy_url=self._proxy_url,
+                                    proxy_port=self._proxy_port,
+                                    initialize=False))
                 else:
                     setattr(self, "_"+ k, v)
             else:
@@ -223,3 +232,208 @@ class Version(BaseAGSServer):
         if self._ancestorVersions is None:
             self.__init()
         return self._ancestorVersions
+
+########################################################################
+class Replica(BaseAGSServer):
+    """represents a replica in a geodata service"""
+
+    _securityHandler = None
+    _proxy_port = None
+    _proxy_url = None
+    _url = None
+
+    _name = None
+    _id = None
+    _replicaVersion = None
+    _guid = None
+    _role = None
+    _replicaOwner = None
+    _serviceName = None
+    _accessType = None
+    _myGenerationNumber = None
+    _sibGenerationNumber = None
+    _sibMyGenerationNumber = None
+    _replicaState = None
+    _sibConnectionString = None
+    _modelType = None
+    _singleGeneration = None
+    _spatialRelation = None
+    _queryGeometryType = None
+    _queryGeometry = None
+    _transferRelatedObjects = None
+    _layers = None
+    _reconcilePolicy = None
+
+    #----------------------------------------------------------------------
+    def __init__(self, url, securityHandler=None,
+                 proxy_url=None, proxy_port=None,
+                 initialize=False):
+        """Constructor"""
+        self._url = url
+        self._securityHandler = securityHandler
+        if not securityHandler is None:
+            self._referer_url = securityHandler.referer_url
+        self._proxy_port = proxy_port
+        self._proxy_url = proxy_url
+        if initialize:
+            self.__init()
+    #----------------------------------------------------------------------
+    def __init(self):
+        """ initializes all the properties """
+        params = {
+            "f" : "json"
+        }
+        json_dict = self._do_get(url=self._url, param_dict=params,
+                                 securityHandler=self._securityHandler,
+                                 proxy_url=self._proxy_url,
+                                 proxy_port=self._proxy_port)
+        attributes = [attr for attr in dir(self)
+                      if not attr.startswith('__') and \
+                      not attr.startswith('_')]
+        for k,v in json_dict.iteritems():
+            if k in attributes:
+                setattr(self, "_"+ k, json_dict[k])
+            else:
+                print k, " - attribute not implemented in Replica."
+
+    #----------------------------------------------------------------------
+    def __str__(self):
+        """returns object as string"""
+        if self._json is None:
+            self.__init()
+        return self._json
+    #----------------------------------------------------------------------
+    def __iter__(self):
+        """
+        returns key/value pair
+        """
+        attributes = json.loads(str(self))
+        for att in attributes.keys():
+            yield [att, getattr(self, att)]
+
+    #----------------------------------------------------------------------
+    @property
+    def name(self):
+        if self._name is None:
+            self.__init()
+        return self._name
+    #----------------------------------------------------------------------
+    @property
+    def sibGenerationNumber(self):
+        if self._sibGenerationNumber is None:
+            self.__init()
+        return self._sibGenerationNumber
+    #----------------------------------------------------------------------
+    @property
+    def queryGeometry(self):
+        if self._queryGeometry is None:
+            self.__init()
+        return self._queryGeometry
+    #----------------------------------------------------------------------
+    @property
+    def singleGeneration(self):
+        if self._singleGeneration is None:
+            self.__init()
+        return self._singleGeneration
+    #----------------------------------------------------------------------
+    @property
+    def modelType(self):
+        if self._modelType is None:
+            self.__init()
+        return self._modelType
+    #----------------------------------------------------------------------
+    @property
+    def layers(self):
+        if self._layers is None:
+            self.__init()
+        return self._layers
+    #----------------------------------------------------------------------
+    @property
+    def queryGeometryType(self):
+        if self._queryGeometryType is None:
+            self.__init()
+        return self._queryGeometryType
+    #----------------------------------------------------------------------
+    @property
+    def accessType(self):
+        if self._accessType is None:
+            self.__init()
+        return self._accessType
+    #----------------------------------------------------------------------
+    @property
+    def replicaVersion(self):
+        if self._replicaVersion is None:
+            self.__init()
+        return self._replicaVersion
+    #----------------------------------------------------------------------
+    @property
+    def spatialRelation(self):
+        if self._spatialRelation is None:
+            self.__init()
+        return self._spatialRelation
+    #----------------------------------------------------------------------
+    @property
+    def transferRelatedObjects(self):
+        if self._transferRelatedObjects is None:
+            self.__init()
+        return self._transferRelatedObjects
+    #----------------------------------------------------------------------
+    @property
+    def serviceName(self):
+        if self._serviceName is None:
+            self.__init()
+        return self._serviceName
+    #----------------------------------------------------------------------
+    @property
+    def role(self):
+        if self._role is None:
+            self.__init()
+        return self._role
+    #----------------------------------------------------------------------
+    @property
+    def myGenerationNumber(self):
+        if self._myGenerationNumber is None:
+            self.__init()
+        return self._myGenerationNumber
+    #----------------------------------------------------------------------
+    @property
+    def replicaState(self):
+        if self._replicaState is None:
+            self.__init()
+        return self._replicaState
+    #----------------------------------------------------------------------
+    @property
+    def sibConnectionString(self):
+        if self._sibConnectionString is None:
+            self.__init()
+        return self._sibConnectionString
+    #----------------------------------------------------------------------
+    @property
+    def guid(self):
+        if self._guid is None:
+            self.__init()
+        return self._guid
+    #----------------------------------------------------------------------
+    @property
+    def sibMyGenerationNumber(self):
+        if self._sibMyGenerationNumber is None:
+            self.__init()
+        return self._sibMyGenerationNumber
+    #----------------------------------------------------------------------
+    @property
+    def id(self):
+        if self._id is None:
+            self.__init()
+        return self._id
+    #----------------------------------------------------------------------
+    @property
+    def reconcilePolicy(self):
+        if self._reconcilePolicy is None:
+            self.__init()
+        return self._reconcilePolicy
+    #----------------------------------------------------------------------
+    @property
+    def replicaOwner(self):
+        if self._replicaOwner is None:
+            self.__init()
+        return self._replicaOwner
