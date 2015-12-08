edc47f8fb69757d4ab36842dea4bbb4e660525db
Andrew <achapkowski@esri.com>
2015-12-08 13:06:32 -0500

    fixed print error on ags/services.py where print was not set to the print()

--- src/arcrest/ags/services.py
+++ src/arcrest/ags/services.py
@@ -79,7 +79,7 @@ class FeatureService(BaseAGSServer):
             if k in attributes:
                 setattr(self, "_"+ k, v)
             else:
-                print k, " - attribute not implemented for Feature Service."
+                print(k, " - attribute not implemented for Feature Service.")
     #----------------------------------------------------------------------
     @property
     def administration(self):
