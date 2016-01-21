1d3b5abd423149b1851142e2e32d21e18fba69c5
Andrew <achapkowski@esri.com>
2016-01-21 10:20:16 -0500

    updated NTLM3 to point to six.py in the python package, not the site-package install of six.py

--- src/arcrest/security/ntlm3/HTTPNtlmAuthHandler.py
+++ src/arcrest/security/ntlm3/HTTPNtlmAuthHandler.py
@@ -11,8 +11,8 @@
 # You should have received a copy of the GNU Lesser General Public
 # License along with this library.  If not, see <http://www.gnu.org/licenses/> or <http://www.gnu.org/licenses/lgpl.txt>.
 
-from six.moves import urllib
-from six.moves.http_client import HTTPConnection, HTTPSConnection
+from ...packages.six.moves import urllib
+from ...packages.six.moves.http_client import HTTPConnection, HTTPSConnection
 
 import socket
 import re
