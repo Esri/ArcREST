433b40d503ce0a76c3340bc921f69bfe3701f366
Andrew Turner <aturner@esri.com>
2016-01-30 20:58:11 -0500

    Removed commented out geocoder
    Thought I'm not sure if this is used anywhere?

--- src/arcrest/agol/helperservices/__init__.py
+++ src/arcrest/agol/helperservices/__init__.py
@@ -2,5 +2,5 @@ from __future__ import absolute_import
 from .analysis import *
 from .elevation import *
 from .hydrology import *
-# from .geocoder import *
+from .geocoder import *
 __version__ = "3.5.3"
