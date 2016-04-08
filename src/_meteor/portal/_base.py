a1d104f2f5f97222e7f44fa350c5b30d101a45f8
Andrew <achapkowski@esri.com>
2016-04-08 14:30:09 -0400

    Added _base.py, update localportal/__init__.py and administration.py

---
+++ src/_meteor/portal/_base.py
@@ -0,0 +1,48 @@
+from __future__ import absolute_import
+import json
+###########################################################################
+class BasePortal(object):
+    _url = None
+    _con = None
+    _json_dict = None
+    def __init__(self, connection, url, initialize=False):
+        """constructor"""
+        self._con = connection
+        self._url = url
+        self._json_dict = None
+        if initialize:
+            self.__init(connection)
+    #----------------------------------------------------------------------
+    def __init(self, connection):
+        """loads the properties"""
+        params = {"f" : "json"}
+        result = connection.get(path_or_url=self._url, params=params)
+        if isinstance(result, dict):
+            self._json_dict = result
+            for k,v in result.items():
+                setattr(self, k, v)
+                del k,v
+        else:
+            raise RuntimeError("Could not connect to the service: %s" % result)
+    #----------------------------------------------------------------------
+    def refresh(self):
+        """reloads all the services properties"""
+        self.__init(connection=self._con)
+    #----------------------------------------------------------------------
+    def __str__(self):
+        """object as string"""
+        if self._json_dict is None:
+            self.__init(connection=self._con)
+        return json.dumps(self._json_dict)
+    #----------------------------------------------------------------------
+    def __repr__(self):
+        """representation object"""
+        return "{classtype}({data})".format(
+            classtype=self.__class__.__name__,
+            data=self.__str__())
+    #----------------------------------------------------------------------
+    def __iter__(self):
+        """creates iterable for classes properties"""
+        for k,v in self.__dict__.items():
+            yield k,v
+
---
+++ src/_meteor/portal/localportal/__init__.py
@@ -0,0 +1,6 @@
+from __future__ import absolute_import
+from . import administration
+
+__version__ = "4.0.0"
+
+__all__ = ['administration']
