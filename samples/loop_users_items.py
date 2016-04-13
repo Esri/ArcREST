8a3b221e65055e9fad472d5534b781b3fa07c97c
Paul LeBlanc <PLeBlanc@esri.com>
2016-04-13 15:52:17 -0700

    loop users items
    print out error on usage report

--- samples/loop_users_items.py
+++ samples/loop_users_items.py
@@ -17,6 +17,7 @@ createUsage = True # to filter views by date
 # Start & end times to aggregate between
 start = dt(2015, 7, 1)
 end = dt(2015, 9, 1)
+# Change period to different value (1w, 1m, etc.) for larger date ranges.
 params = {
             "period": "1d",
             "groupby": "name",
@@ -29,7 +30,7 @@ if __name__ == "__main__":
     username = ""#Username
     password = ""#password
     proxy_port = None
-    proxy_url = None   
+    proxy_url = None
 
     agolSH = AGOLTokenSecurityHandler(username=username,
                                       password=password)
@@ -39,33 +40,39 @@ if __name__ == "__main__":
     content = admin.content
     portal = admin.portals.portalSelf
     commUsers = portal.users(start=1, num=100)
-    commUsers = commUsers['users']    
-    
+    commUsers = commUsers['users']
+
     for commUser in commUsers:
         user = admin.content.users.user(commUser.username)
-        for userItem in user.items:                
-        
+        for userItem in user.items:
+
             msg = "Item: {0}".format(userItem.id)
             msg = msg +  "\n\tName: {0}".format(userItem.name)
             msg = msg +  "\n\tTitle: {0}".format(userItem.title)
+            msg = msg +  "\n\tType: {0}".format(userItem.type)
             msg = msg +  "\n\tOwned by: {0}".format(commUser.username)
-            msg = msg +  "\n\tCreated on: {0}".format(arcrest.general.online_time_to_string(userItem.modified,datetimeformat))
+            msg = msg +  "\n\tCreated on: {0}".format(arcrest.general.online_time_to_string(userItem.created,datetimeformat))
             msg = msg +  "\n\tLast modified on: {0}".format(arcrest.general.online_time_to_string(userItem.modified,datetimeformat))
             msg = msg +  "\n\tTotal Views: {0}".format(userItem.numViews)
-            
+
             if createUsage:
-                params['name'] = item.id
+                params['name'] = userItem.id
                 usage = portal.usage(start, end, **params)
                 views = 0
-                for data in usage['data']:
-                    # Views per period are returned as a list of lists
-                    # Import to numpy for easy summing
-                    arr = np.array(data['num'], dtype=np.uint64)
-                    views = arr.sum(0)[1] # Sum "2nd column"
-                    # Alternatively, without numpy
-                    # views = sum(map(int, zip(*data['num'])[1]))
+
+                if usage.get('error', None) is None:
+                    for data in usage.get('data', []):
+                        # Views per period are returned as a list of lists
+                        # Import to numpy for easy summing
+                        arr = np.array(data['num'], dtype=np.uint64)
+                        views = arr.sum(0)[1] # Sum "2nd column"
+                        # Alternatively, without numpy
+                        # views = sum(map(int, zip(*data['num'])[1]))
+                else:
+                    print(usage['error'])
+
                 msg = msg +  "\n\tTimestamped Views: {0}".format(views)
-            
+
             msg = msg +  "\n----------------------------------------------------------"
             print (msg)
-            
+
