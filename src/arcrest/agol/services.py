dcdc4cb1a7d03e64c44352cb9df0c6715743b997
achapkowski <achapkowski@esri.com>
2016-05-12 09:03:00 -0400

    fix logic statement on query to better handle returnCount parameter.

--- src/arcrest/agol/services.py
+++ src/arcrest/agol/services.py
@@ -1771,7 +1771,8 @@ class FeatureLayer(abstract.BaseAGOLClass):
                             proxy_port=self._proxy_port)
         if 'error' in result:
             raise ValueError(result) 
-        if as_json:
+        if as_json or \
+           returnCountOnly == True:
             return result
         elif returnFeatureClass and\
              not returnCountOnly and \
